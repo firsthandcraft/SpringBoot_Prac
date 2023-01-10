f734771dfc547adb4abdbe4fcde72bd090b2c412
firsthandcraft <opaldust19@gmail.com>
2023-01-10 10:53:03 +0900

    0110

---
+++ 00.springbootprac04/src/main/java/com/study/springboot/Member.java
@@ -0,0 +1,31 @@
+package com.study.springboot;
+
+public class Member {
+	private String id;
+	private String name;
+	public Member(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public Member() {
+		super();
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", name=" + name + "]";
+	}
+	
+}
