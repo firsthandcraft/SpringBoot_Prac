2aa92d7c3b77fa6fa0f8936eb3b2d8276da07967
handcraft <97497153+firsthandcraft@users.noreply.github.com>
2023-02-01 17:36:00 +0900

    Add files via upload
    requestmappping

---
+++ 02Ex8-1/Ex8-1/src/main/java/com/study/springboot/Ex81Application.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex81Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex81Application.class, args);
+	}
+
+}
